2d780bf3b58e96d73f16ada88c21634774fbd483
jazzy10127 <78111920+jazzy10127@users.noreply.github.com>
2022-02-13 23:38:49 -0800

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    jasmine
+ * Created:   02.13.2022
+ *
+ * (c) Copyright by Jasmine
+ **/
+
+ //define variables
+ make="hondai";
+ model="elantra";
+ color="red";
+ year="2020";
+
+//calculate
+age = 2020 - year;
+
+//output
+document.writeln("make: " + make + "<br>");
+document.writeln("model: " + model + "<br>");
+document.writeln("color: " + color + "<br>");
+document.writeln("year: " + year + "<br>");
+document.writeln("age: " + age + "<br>");
