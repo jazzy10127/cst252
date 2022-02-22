be4fe3e491d05694bee7be367c8b59b2eb19d2cc
jazzy10127 <78111920+jazzy10127@users.noreply.github.com>
2022-02-22 12:41:38 -0800

    lab 7

---
+++ lab7/js/lab.js
@@ -0,0 +1,30 @@
+/**
+ * Author:    Name
+ * Created:   11.05.2009
+ *
+ * (c) Copyright by Blub Corp.
+ **/
+
+ // sortUserName - a function user input and sort the letters
+ // of their name
+ function sortUserName() {
+   var userName = window.prompt("Please enter name to continue...");
+   console.log("userName =", userName);
+   // split string to array
+   var nameArray = userName.split('');
+   console.log("nameArray =", nameArray);
+   // sort the array
+   var nameArraySort = nameArray.sort();
+   console.log("nameArraySort =", nameArraySort);
+   // join array back to a string
+   var nameSorted = nameArraySort.join('');
+   console.log("nameSorted =", nameSorted);
+   //  userName.toLower().split("").sort().join("")
+   return nameSorted;
+ }
+
+ //output
+ document.writeln("This looks better :)")
+  sortUserName(), "</br>";
+  document.writeln("sortUserName <pre>",
+    JSON.stringify(sortUserName, null, '\t'), "</pre>");
