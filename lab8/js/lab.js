551155a5d04da050c85c9b21472e1241176f36c6
jazzy10127 <78111920+jazzy10127@users.noreply.github.com>
2022-02-23 11:32:26 -0800

    lab 8

---
+++ lab8/js/lab.js
@@ -0,0 +1,30 @@
+/**
+ * Author:   jasmine hernandez
+ * Created:   02/22/22
+ *
+ * license: public domain
+ **/
+
+
+ function sortUserName() {
+   var userName = window.prompt("Please enter name to continue...");
+   console.log("userName =", userName);
+
+   var nameArray = userName.split('');
+   console.log("nameArraySort =", nameArray);
+
+   var nameArraySort = nameArray.sort();
+   console.log("nameArraySort =", nameArraySort);
+
+   var nameSorted = nameArraySort.join('');
+   console.log("nameSorted =", nameSorted);
+
+   return nameSorted;
+ }
+
+ document.writeln("This looks better: ",
+  sortUserName(), "</br>");
+ //output
+ //document.writeln("Kinds of transportation I use: ", mytransport, "</br>";
+ //document.writeln("My Main Ride: <pre>",
+   //JSON.stringify(myMainRide, null, '\t'), "</pre>");
