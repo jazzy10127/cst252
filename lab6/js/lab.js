53d0c7fe4201310ee47d636f323717964d5094cb
jazzy10127 <78111920+jazzy10127@users.noreply.github.com>
2022-02-27 11:07:46 -0800

    lab 6

---
+++ lab6/js/lab.js
@@ -0,0 +1,25 @@
+/**
+ * Author:    Name
+ * Created:   11.05.2009
+ *
+ * (c) Copyright by Blub Corp.
+ **/
+
+ // Define variables
+
+ var mytransport = ["hondai-elantra", "MST", "american-airlines", "lyft", "allstar-adidas"];
+
+ var myMainRide = {
+    make: "Hondai",
+    model: "elantra",
+    color: "red",
+    year: 2020,
+    age: function() {
+        return 2022 - this.year;
+        }
+}
+
+// output
+document.writeln("Kinds of transportation I use: ", mytransport, "</br>";
+document.writeln("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
