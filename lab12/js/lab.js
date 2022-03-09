5abe893244c01200953ca3becf3dd22746716113
jazzy10127 <78111920+jazzy10127@users.noreply.github.com>
2022-03-09 12:10:43 -0800

    lab 12

---
+++ lab12/js/lab.js
@@ -0,0 +1,13 @@
+/**
+ * Author:   jasmine hernandez
+ * Created:   03/13/22
+ *
+ * license: public domain
+ **/
+
+ $("#input-button").click(function(){
+ 	var inputText = $("#input-text").val();
+   $("#output").append("<p>" + inputText);
+
+ })
+//output
