25942879fcec966af3be07f94fd2bd5393eaefe3
jazzy10127 <78111920+jazzy10127@users.noreply.github.com>
2022-03-14 13:06:12 -0700

    lab 13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -5,9 +5,34 @@
  * license: public domain
  **/
 
- $("#input-button").click(function(){
- 	var inputText = $("#input-text").val();
-   $("#output").append("<p>" + inputText);
+ function sortingHat(name) {
+  //get length of name
+  var len = name.length;
+  // do a mod 4 to get house
+  var mod = len % 4;
+  //if conditional to set houseStr to house
+  if (mod == 0){
+    houseStr = "Doctor Strange";
+  } else if (mod == 1){
+    houseStr = "Spiderman";
+  } else if (mod == 2){
+  	houseStr = "Black widow";
+  } else if (mod == 3){
+  	houseStr = "Gamora";
+  }
 
- })
+  //return house txt
+  return houseStr;
+}
+
+$("#my-button").click(function(){
+var name = $("#input").val();
+var house = sortingHat(name);
+console.log(house);
+$("#output").append("<p>" + house);
+})
+
+var name = "jasmine h";
+var name = sortingHat(name);
+console.log(house);
 //output
