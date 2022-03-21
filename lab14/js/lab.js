59badb0244d3b3fda8f219471912f56899965410
jazzy10127 <78111920+jazzy10127@users.noreply.github.com>
2022-03-20 21:00:41 -0700

    lab 14

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -5,5 +5,26 @@
  * license: public domain
  **/
 
+for (let i = 1; i <=200; i++) {
+  var buildStr = "";
+ if(i % 3 ==0) {
+    buildStr += "Fizz";
+  }
+  if( i % 5 == 0) {
+    buildStr += "Buzz!";
+  }
+  if( i % 7 == 0) {
+    buildStr += "Boom!";
+  }
 
+  if(buildStr !=""){
+    console.log(buildStr + "!");
+    $("#output").append("<p>" + buildStr + "!");
+  }
+  else {
+    console.log(i);
+    $("#output").append("<p>" + i);
+  }
+  }
+}
 //output
